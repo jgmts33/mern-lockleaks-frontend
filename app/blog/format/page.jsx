22bf24a7c70f0bc824e587013ee70722066794eb
jgmts33 <jorgreemts@gmail.com>
2024-03-06 06:14:44 +0300

    update: js file name to jsx for more truth

---
+++ app/blog/format/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+import React from 'react'
+import {
+    Button
+} from '@nextui-org/react';
+import { useEffect, useState } from 'react';
+import { ChevronRight } from "@/src/utils/Icons";
+
+export default function BlogFormat() {
+    const icons = {
+        right: <ChevronRight fill="currentColor" size={16} />,
+    };
+
+    const article_content = {
+        title:"ARTICLE TITLE",
+        description:"Lorem ipsum dolor sit amet consectetur. Tellus nullam a nunc euismod. Suspendisse orci diam elementum faucibus natoque odio blandit. Neque sed vivamus elementum vitae nulla leo id sem pharetra. Amet viverra purus consectetur enim sit consequat velit amet. Orci nec volutpat mi tortor. Blandit urna vel consectetur aliquam id malesuada sit gravida pulvinar. Ultricies suscipit malesuada nunc eu. Iaculis etiam morbi sit faucibus. Integer lectus at amet amet quisque neque nunc. Est id ipsum accumsan ullamcorper. Urna sit quis id pellentesque diam turpis elit erat. Morbi enim blandit ultrices laoreet dapibus neque. At sit id amet at in. In maecenas nisl luctus odio nisi. Vestibulum id bibendum nec lectus aenean duis. Amet egestas interdum risus sem turpis vestibulum eu. In vulputate eu mi fringilla tellus ut proin nunc. Cursus ipsum enim erat amet neque gravida. At malesuada neque quam pharetra id tristique sagittis scelerisque.",
+    }
+
+    return (
+        <div className="px-10 text-white max-w-[1480px] py-4">
+
+        </div>
+    )
+}
