4d2f2f613c070f569f2cc148f27bc8103f548e5e
jgmts33 <jorgreemts@gmail.com>
2024-03-30 01:06:25 +0300

    fix:space

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -29,7 +29,7 @@ export default function Login() {
     useEffect(() => {
         setEmailError("");
         setPasswordError("")
-    }, [email, password])
+    }, [email])
 
     const handleLogin = () => {
         if (email == "") {
