38327e03f0aebd26766fad939e41d83a8c18123f
jgmts33 <jorgreemts@gmail.com>
2024-03-30 00:57:44 +0300

    fix:services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -82,7 +82,7 @@ export default function SERVICES() {
                     {
                         ServicesContent.map((item, index) => {
                             return (
-                                <div key={index} className={(index == 1 ? 'mt-20 pt-10 max-lg:-mt-12' : "") + " max-w-[480px] max-lg:mt-1 flex flex-col gap-4" }>
+                                <div key={index} className={(index == 1 ? 'mt-20 pt-10 max-lg:-mt-10' : "") + " max-w-[480px] max-lg:mt-1 flex flex-col gap-4" }>
                                     {
                                         item.map((service_content, count) => {
                                             return (
