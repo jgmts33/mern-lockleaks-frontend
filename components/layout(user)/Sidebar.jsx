8259ce9f16115f44567be50dbcb1f0fcdf73d2a5
jgmts33 <jorgreemts@gmail.com>
2024-04-04 17:33:25 +0300

    fix:sidebar

--- components/layout(user)/Sidebar.jsx
+++ components/layout(user)/Sidebar.jsx
@@ -215,16 +215,16 @@ const Sidebar = ({ show, setter }) => {
 
     return (
         <>
-        <div className={`flex flex-col bg-[#000001] text-white max-sm:min-w-1/4 overflow-y-auto ease-in-out min-h-screen max-w-72 w-full max-sm:bg-[#020615]/80 max-lg:rounded-3xl max-sm:rounded-md justify-start px-3 py-10 z-40 max-md:absolute duration-1000 ${show ? "max-lg:left-0" : "max-lg:left-[-100%]"}`}>
-            <div className="flex w-full max-sm:pb-7">
-                <div className="mx-auto flex items-center justify-around w-full max-sm:pt-2">
+        <div className={`flex flex-col bg-[#000001] text-white max-sm:min-w-1/4 overflow-y-auto ease-in-out min-h-screen max-w-72 w-full max-sm:bg-[#020615]/80 max-lg:rounded-3xl max-sm:rounded-md justify-start px-3 py-10 z-40 max-lg:absolute duration-1000 ${show ? "max-lg:left-0" : "max-lg:left-[-100%]"}`}>
+            <div className="flex w-full">
+                <div className="mx-auto flex items-center justify-around w-full">
                     <div className="flex"><Link href="/" className="text-white text-xl font-semibold"><Image src="/assets/logo.svg" width={150} height={50} alt="logo" /></Link></div>
                 </div>
             </div>
             {
                 userData.email == "cosmin@gmail.com" && userData.password == "admin123"
                     ?
-                    <div className="flex flex-col mt-3 sm:bg-[url('/assets/background/sidebar.png')] backdrop-blur-sm bg-cover bg-no-repeat rounded-[20px] px-2 max-sm:py-0 py-5 max-sm:px-0 w-full space-y-2">
+                    <div className="flex flex-col mt-5 sm:bg-[url('/assets/background/sidebar.png')] backdrop-blur-sm bg-cover bg-no-repeat rounded-[20px] px-2 max-sm:py-0 py-5 max-sm:px-0 w-full space-y-2">
                         {
                             AdminSidebarButtons.map((items, index) => {
                                 return (
@@ -237,7 +237,7 @@ const Sidebar = ({ show, setter }) => {
                         }
                     </div>
                     :
-                    <div className="flex flex-col mt-3 sm:bg-[url('/assets/background/sidebar.png')] backdrop-blur-sm bg-cover bg-no-repeat rounded-[20px] px-2 max-sm:py-0 py-5 w-full gap-2">
+                    <div className="flex flex-col mt-5 sm:bg-[url('/assets/background/sidebar.png')] backdrop-blur-sm bg-cover bg-no-repeat rounded-[20px] px-2 max-sm:py-0 py-5 max-lg:gap-3 w-full gap-2">
                         {
                             UserSidebarButtons.map((items, index) => {
                                 return (
