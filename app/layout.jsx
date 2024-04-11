c9ebdbdee524206597ae0df3f181b50a6cf70e47
jgmts33 <jorgreemts@gmail.com>
2024-04-11 04:14:54 +0300

    fix:user panel design

--- app/layout.jsx
+++ app/layout.jsx
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
                   <Sidebar show={showSidebar} setter={setShowSidebar} />
                   <div className="w-full gradiant-background">
                     <UserHeader setter={setShowSidebar} />
-                    <div className="flex flex-col flex-grow md:w-full h-[calc(100vh-65px)] overflow-y-auto" style={{scrollBehavior:'smooth'}}>
+                    <div className="flex flex-col flex-grow md:w-full h-[calc(100vh-64px)] overflow-y-auto" style={{scrollBehavior:'smooth'}}>
                       {children}
                     </div>
                   </div>
