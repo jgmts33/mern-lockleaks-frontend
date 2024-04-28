3bddbd25942d7462ff2d3315582f5aaa29fc9ac5
jgmts33 <jorgreemts@gmail.com>
2024-04-28 23:38:36 +0100

    update: callback_urls for facebook and twitter

--- components/auth/twitter.jsx
+++ components/auth/twitter.jsx
@@ -19,7 +19,7 @@ export default function FaceBookAuth() {
 
     const stringifiedParams = queryString.stringify({
       client_id: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID,
-      redirect_uri: 'https://copyrightfixer.com/auth/twitter/',
+      redirect_uri: 'https://copyrightfixer.com/auth/twitter',
       state: 'state',
       response_type: 'code',
       code_challenge: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET,
