9889b5097b92ee126714b19f5924ece0a83c3d58
jgmts33 <jorgreemts@gmail.com>
2024-04-29 10:30:10 +0100

    fix: tiwtterAuth page

--- components/auth/twitter.jsx
+++ components/auth/twitter.jsx
@@ -22,7 +22,7 @@ export default function FaceBookAuth() {
     const stringifiedParams = queryString.stringify({
       client_id: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID,
       redirect_uri: 'https://copyrightfixer.com/auth/twitter',
-      state: 'state',
+      state: 'twitter-increaser-state',
       response_type: 'code',
       code_challenge: 'challenge',
       code_challenge_method: 'plain',
