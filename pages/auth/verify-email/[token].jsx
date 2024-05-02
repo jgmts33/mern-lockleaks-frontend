b719b461c3869f19bc4a6d829fe5775d7312cdb6
jgmts33 <jorgreemts@gmail.com>
2024-05-02 13:58:44 +0100

    update: authentication verifing

--- pages/auth/verify-email/[token].jsx
+++ pages/auth/verify-email/[token].jsx
@@ -1,10 +1,12 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { verifyEmail } from "@/axios/auth";
+import { useDispatch } from "react-redux";
 
 export default function VerifyEmail() {
 
   const router = useRouter();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     (async () => {
@@ -12,6 +14,7 @@ export default function VerifyEmail() {
         const res = await verifyEmail(router.query.token);
 
         if (res.status === 'success') {
+          dispatch(setUserInfo({ ...res.data }));
           // Email verification successful
           router.push('/app/dashboard');
         }
